d6751e59835f2de6e57ea542f905eca9b2e8f195
Zihao Li <zl81@st-andrews.ac.uk>
2023-02-21 21:57:44 +0000

    Fix an entry in the repo-topic source file and update the script to allow topic selection through command line argument

--- data/repo_topic.py
+++ data/repo_topic.py
@@ -1,23 +1,33 @@
 # This script is used to generate a dictionary of repo-topic pairs from the repo_topic.json file
 # Repo-topic Source: https://github.com/vinta/awesome-python/blob/master/README.md
 import json
+import argparse
 from pathlib import Path
 
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    "-t",
+    "--topics",
+    nargs="+",
+    help="Select topics to get corresponding repos",
+    default=None,
+)
+
+topics = parser.parse_args().topics
+
+data = {}
 with open(Path(__file__).with_name("repo_topic.json")) as f:
     data = json.load(f)
+    if topics is None:
+        topics = list(data.keys())
+print(f"Selected topics:\n{topics}\n")
 
-# Specify topics
-topics = [
-    "Algorithms",
-    "Audio",
-    "OAuth",
-    "Cryptography",
-    "Downloader",
-    "PDF",
-    "Markdown",
-]
+repo_topic = {}
+for topic in topics:
+    if topic not in data:
+        print(f"[-] Topic '{topic}' not found")
+        exit(1)
+    repo_topic.update({repo: topic for repo in data[topic]})
 
-repo_topic = {repo: topic for topic in topics for repo in data[topic]}
-
-print(f"Selected topics:\n{topics}")
 print(f"Repo-topic pairs:\n{repo_topic}")
